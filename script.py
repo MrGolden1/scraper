3339786e67fcd73f38e77610b8201d79559ea938
MrGolden <ali.zarrinzadeh@gmail.com>
2022-05-18 22:41:50 +0430

    some comments added

--- script.py
+++ script.py
@@ -5,17 +5,19 @@ import threading
 import queue
 import time
 
+# journal_api: through this api, we can get list of journals
+# journals are paginated and every page has 10 journals, so we need to request every page to get all journals
 journals_api = "https://search-app.prod.ecommerce.elsevier.com/api/search?labels=journals&page={}&in-publication=true&locale=global"
+# journal_info_api: through this api, we can get journal info where we can find the link to guide for authors
 journal_info_api = "https://www.journals.elsevier.com/_next/data/OohGP-kM0hfp4_xLmWgAq/{}.json"
 
 keyword = "Your Paper Your Way"
+# target_journals: all pages that contain keyword will be added to this list
 target_journals = []
 
 total_pages = None
 current_page = 1
 
-lock = threading.Lock()
-html_queue = queue.Queue()
 
 # dummy request to get total number of pages
 r = requests.get(journals_api.format(current_page))
@@ -25,10 +27,15 @@ else:
     print("Error: {}".format(r.status_code))
     exit(1)
     
+lock = threading.Lock()
+html_queue = queue.Queue()
 
+# here we have to funcion scrap all pages and journals
+# get_html: in a while loop, in each iteration, it get all journals in a page and add them to html_queue
 def get_html():
     global current_page
     while True:
+        # to provide thread safety, lock is used
         with lock:
             i = current_page
             current_page += 1
@@ -36,11 +43,11 @@ def get_html():
             break
         
         print("Page {}/{}".format(i, total_pages))
-        r = requests.get(journals_api.format(i))
-        if r.status_code == 200:
-            journals = json.loads(r.text)["hits"]
+        page_r = requests.get(journals_api.format(i))
+        if page_r.status_code == 200:
+            journals = json.loads(page_r.text)["hits"]
         else:
-            print("Error: {}".format(r.status_code))
+            print("Error: {}".format(page_r.status_code))
             exit(1)
             
         for journal in journals:
@@ -57,7 +64,9 @@ def get_html():
                 guide_for_authors_url = info["pageProps"]["publishingOptions"]["guide_for_authors_url"]
                 html = requests.get(guide_for_authors_url).text
                 html_queue.put((title, guide_for_authors_url, html))
-    
+
+# check_keyword: simple checks if the html contains keyword
+# if it does, it adds the journal to target_journals
 def check_keyword():
     while True:
         item = html_queue.get()
@@ -70,6 +79,8 @@ def check_keyword():
             target_journals.append((title, guide_for_authors_url))
 
 start_time = time.time()
+# to get best performance, we need to run this script in multiple threads
+# 25 threads is sensible to avoid overhead and getting blocked
 THREAD_NUM = 25
 threads = []
 for i in range(THREAD_NUM):
